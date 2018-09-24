f13261e550f0258b27baa04936aa79c0c5f464a1
Andrew Craigie <andrew@craigie.co>
2018-09-24 15:21:55 +0100

    Initial Commit

---
+++ src/test/java/BearTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class BearTest {
+
+    Bear bear;
+
+    @Before
+    public void before(){
+        bear = new Bear("Balloo", 25, 96.62);
+    }
+
+    @Test
+    public void hasName(){
+        String name = bear.getName();
+        assertEquals("Balloo", name);
+    }
+
+    @Test
+    public void hasAge(){
+        int age = bear.getAge();
+        assertEquals(25, age);
+    }
+
+    @Test
+    public void hasWeight(){
+        double weight = bear.getWeight();
+        assertEquals(96.62, weight, 0.01);
+    }
+
+    @Test
+    public void readyToHibernateIfGreaterThan80(){
+        boolean readyToHibernate = bear.isReadyToHibernate();
+        assertEquals(true, readyToHibernate);
+    }
+
+    @Test
+    public void notReadyToHibernateIfLessThan80(){
+        Bear thinBear = new Bear("Yogi", 25, 65.44);
+        boolean readyToHibernate = thinBear.isReadyToHibernate();
+        assertEquals(false, readyToHibernate);
+    }
+
+}
